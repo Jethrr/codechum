4c58830b778629b3479d1003edee14696777c8e0
Jether Omictin <jethomictin@gmail.com>
2024-02-14 08:16:04 +0800

    initial commit

--- loginDashboard/src/components/Navbar.jsx
+++ loginDashboard/src/components/Navbar.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="flex p-10">
-      <div className="flex ml-2.5 text-3xl font-bold">
+    <div className="flex p-5">
+      <img src="/images/cody.png" className="w-24 ml-5" />
+      <div className="flex  text-3xl font-bold justify-center items-center">
         <h2 className="text-blue-400">CODE</h2>
         <h2>CHUM</h2>
       </div>
